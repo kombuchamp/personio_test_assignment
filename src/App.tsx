3530b989cfe9d641c552fc878f281fa0f7d0feae
Konstantin Belousov <kombuchamp@yandex-team.ru>
2022-05-06 22:20:59 +0300

    bootstrap App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
-import { ThemeProvider } from '@mui/material';
+import React, { FC } from 'react';
+import { Grid, ThemeProvider } from '@mui/material';
 import { DARK_THEME, LIGHT_THEME } from './const/themes';
 import { useTypedSelector } from './hooks/redux-helpers';
+import { Header } from './components/Header';
+import { TableFilters } from './components/TableFilters';
+import { Content } from './components/Content';
 
-function App() {
+export const App: FC = () => {
     const { theme } = useTypedSelector((state) => state.themeReducer);
 
     return (
         <ThemeProvider theme={theme === 'light' ? LIGHT_THEME : DARK_THEME}>
-            <div>it works</div>
+            <Grid
+                container
+                flexDirection={'column'}
+                rowGap={1}
+                sx={{
+                    minHeight: '100vh',
+                    bgcolor: 'background.default',
+                }}
+            >
+                <Grid item xs={12}>
+                    <Header />
+                </Grid>
+                <Grid item container xs={12} justifyContent={'center'}>
+                    <TableFilters />
+                </Grid>
+                <Grid item container xs={12} justifyContent={'center'}>
+                    <Content />
+                </Grid>
+            </Grid>
         </ThemeProvider>
     );
-}
-
-export default App;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { setupStore } from './store/store';
 import { Provider } from 'react-redux';
 import { CssBaseline } from '@mui/material';
+import { App } from './App';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
