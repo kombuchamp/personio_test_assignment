b81a268501dabb9089986c004ff41eeb5230dfef
Konstantin Belousov <kombuchamp@yandex-team.ru>
2022-05-06 21:41:44 +0300

    add redux and theme providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { ThemeProvider } from '@mui/material';
+import { DARK_THEME, LIGHT_THEME } from './const/themes';
+import { useTypedSelector } from './hooks/redux-helpers';
 
 function App() {
-    return <div>it works</div>;
+    const { theme } = useTypedSelector((state) => state.themeReducer);
+
+    return (
+        <ThemeProvider theme={theme === 'light' ? LIGHT_THEME : DARK_THEME}>
+            <div>it works</div>
+        </ThemeProvider>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { setupStore } from './store/store';
+import { Provider } from 'react-redux';
+import { CssBaseline } from '@mui/material';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
+
+const store = setupStore();
+
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <CssBaseline />
+            <App />
+        </Provider>
     </React.StrictMode>
 );
